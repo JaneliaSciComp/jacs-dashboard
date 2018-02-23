e415b40c95ec16f85b519e26afa248209ae32aa8
Jody Clements <jody@neomorphic.co.uk>
2018-02-23 08:35:44 -0500

    Modified the styling and use of material ui.
    Added my own custom styles, that will need to be changed and modified
    the navigation menu.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Reboot from 'material-ui/Reboot';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import purple from 'material-ui/colors/purple';
+import green from 'material-ui/colors/green';
+
 import Navigation from '../containers/Navigation';
 import Home from '../containers/Home';
 import About from '../components/About';
@@ -8,18 +12,31 @@ import NoMatch from '../components/NoMatch';
 import Login from '../containers/Login';
 import PrivateRoute from '../containers/auth/PrivateRoute';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: purple,
+    secondary: green,
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Reboot />
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <PrivateRoute path="/about" component={About} />
-          <Route path="/login" component={Login} />
-          <Route component={NoMatch} />
-        </Switch>
+        <MuiThemeProvider theme={theme}>
+          <Reboot />
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <PrivateRoute path="/about" component={About} />
+            <Route path="/login" component={Login} />
+            <Route component={NoMatch} />
+          </Switch>
+        </MuiThemeProvider>
       </div>
     );
   }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
@@ -6,29 +7,51 @@ import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import { withStyles } from 'material-ui/styles';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
 
 class Navigation extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="title" color="inherit" >
-            JACS v2
-          </Typography>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">
-            <Link to="/">Home</Link>
-          </Button>
-          <Button color="inherit">
-            <Link to="/about/topics" >Topics</Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.flex} variant="title" color="inherit" >
+              JACS v2
+            </Typography>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/about/topics">
+              Topics
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
 
-export default Navigation;
+Navigation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Navigation);
